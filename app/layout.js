ecc43501b0a41a6712ff618c0b0a8e34d512dd89
hritikypandey13-afk <hritikypandey13@gmail.com>
2026-04-20 12:18:43 +0530

    Create app layout

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "LLBAI",
+  description: "Unlock The World Of Law",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
