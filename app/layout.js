d0f0a6f3c00a03dd370a7a4ff17f2b2ae08cb262
hritikypandey13-afk <hritikypandey13@gmail.com>
2026-04-20 12:37:49 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="REmxs3j549C2SiZ4YJsd1RrNKjd77aSbYkctZ-tpuAg" />
+      </head>
       <body>{children}</body>
     </html>
   );
